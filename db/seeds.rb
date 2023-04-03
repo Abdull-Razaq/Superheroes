a37f719734c989c547f8d7e2ea8ef6cebb9003b5
Abdirizak Mohamed <abdirizakm123@gmail.com>
2023-04-03 13:52:34 +0300

    Completed assignment

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+10.times do
+  Hero.create([{ name: Faker::Name.name, super_name: Faker::Superhero.name }])
+end
+
+20.times do
+  Power.create(
+    [
+      {
+        name: Faker::Superhero.power,
+        description: Faker::Quote.yoda,
+      },
+    ],
+  )
+end
+
+10.times do
+  HeroPower.create(
+    [{ strength: %w[Strong Weak Average].sample, hero_id: rand(1..10), power_id: rand(1..20) }],
+  )
+end
